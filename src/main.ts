9d41d177b0a9ea50a905ea5cf979b528537ce1b4
Pedro Augusto da Silva Rosa <pedrorosadev20@gmail.com>
2022-10-18 09:23:22 -0300

    Projeto Base

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common/pipes';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = 4000;
+
+  process.env.TZ = '-03:00';
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
+  await app.listen(port);
+}
+bootstrap();
